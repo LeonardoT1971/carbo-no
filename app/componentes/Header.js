dfd2f630cd576bca3f8b76a0b1b1c09f7f42685b
geomer-simanca <simancanavarrogeomerandres@gmail.com>
2024-11-09 12:15:10 -0500

    creacion del header con estilos

--- app/componentes/Header.js
+++ app/componentes/Header.js
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import logo from '../img/logo.png'
 const Header = () => {
     return(
-        <div className='menu'>
+        <div className='flex flex-col items-center gap-5 p-5 bg-green-50'>
             <Image
                 src={logo}
-                width={500}
+                width={300}
                 height={500}
                 alt="Picture of the author"
             />
-            <div>
+            <div className="text-black w-4/5 flex justify-around">
                 <span>Eco Hoteles</span>
                 <span>Eco Trensporte</span>
                 <span>Calculadora</span>
