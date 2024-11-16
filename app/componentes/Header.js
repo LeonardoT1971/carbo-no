6802f749fae162e11040f38b5369e773c7ef569b
cristianronaldo1 <calborparra@gmail.com>
2024-11-15 20:58:20 -0500

    navbar y hover

--- app/componentes/Header.js
+++ app/componentes/Header.js
@@ -1,25 +1,23 @@
-import { Span } from "next/dist/trace"
-import Image from 'next/image'
-import logo from '../img/logo.png'
-const Header = () => {
-    return(
-        <div className='flex flex-col items-center gap-5 p-5 bg-green-50'>
-            <Image
-                src={logo}
-                width={300}
-                height={500}
-                alt="Picture of the author"
-            />
-            <div className="text-black w-4/5 flex justify-around">
-                <span>Eco Hoteles</span>
-                <span>Eco Trensporte</span>
-                <span>Calculadora</span>
-                <span>Betty Moda</span>
-            </div>
-            
-            
-        </div>
-    )
-}
+import Image from 'next/image';
+import logo from '../img/logo.png';
 
-export default Header
+const Header = () => {
+  return (
+    <header className="flex flex-col items-center gap-5 p-5 bg-green-50 shadow-md">
+      <Image 
+        src={logo} 
+        width={300} 
+        height={80} 
+        alt="Logo de Carbo-NO" 
+      />
+      <nav className="w-full flex justify-around text-lg font-semibold text-black">
+        <a href="#eco-hoteles" className="px-4 py-2 rounded-lg hover:bg-green-600 hover:text-green transition">Eco Hoteles</a>
+        <a href="#eco-transporte" className="px-4 py-2 rounded-lg hover:bg-green-600 hover:text-green transition">Eco Transporte</a>
+        <a href="#calculadora" className="px-4 py-2 rounded-lg hover:bg-green-600 hover:text-green transition">Calculadora</a>
+        <a href="#betty-moda" className="px-4 py-2 rounded-lg hover:bg-green-600 hover:text-green transition">Betty Moda</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
